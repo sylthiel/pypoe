b253c29b3bc8257a8d7b77c173e36c2c041b7a72
Andrey Osh <aoshukov@gmail.com>
2019-09-24 19:31:17 +0300

    debugging item fetcher script to figure out API limitations

--- fourth_project.py
+++ fourth_project.py
@@ -6,7 +6,7 @@ list_of_acctchr=open("C:\\list_of_acctchr.txt", "r", encoding="utf-8")
 current_league_url = "https://www.pathofexile.com/api/ladders/Blight"
 dbg=open("C:\\dbg.log", "w+", encoding="utf-8")
 dbg.write("----------------------------------------------------------")
-dbg.write(f"[{datetime.datetime.now()}] script start")
+dbg.write(f"[{datetime.datetime.now()}] script start\n")
 request=0
 for rank, acct_chr in enumerate(list_of_acctchr):
 	acct, chr = acct_chr.split(",")
@@ -16,7 +16,7 @@ for rank, acct_chr in enumerate(list_of_acctchr):
 	response = requests.get(f"https://www.pathofexile.com/character-window/get-items?accountName={chr}&character={acct}")
 	print(response.status_code)
 	request+=1
-	dbg.write(f"Request iteration {request} for https://www.pathofexile.com/character-window/get-items?accountName={chr}&character={acct} with response code {response.status_code}")
+	dbg.write(f"[{datetime.datetime.now()}] Request iteration {request} for https://www.pathofexile.com/character-window/get-items?accountName={chr}&character={acct} with response code {response.status_code}\n")
 	if(request>=5):
 		time.sleep(2)
 		request=0
