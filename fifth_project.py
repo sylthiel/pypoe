3ae85b1da082d4df6b994c913e03ecb65b48948d
Andrey Osh <aoshukov@gmail.com>
2019-09-26 20:12:14 +0300

    Rebuilding the whole omtherfucking thing from scratch to include classes

--- fifth_project.py
+++ fifth_project.py
@@ -1,5 +1,6 @@
 # This will analyze item data grabbed by fourth_project.py
 import json
+import requests
 
 actual_sorted_link_groups=[]
 
@@ -9,13 +10,36 @@ def allocate_gems(groups, gems):
 	for group in groups:
 		if(len(group) and len(gems)):
 			if (len(gems)>=len(group)):
-				tmp=''.join(sorted((gems[:len(group)])))
+				tmp=''.join((gems[:len(group)]))
 				gems=gems[len(group):]
 				itemgems.append(tmp)
 			elif(len(gems) < len(group)):
 				itemgems.append(''.join(sorted(gems)))
 	return itemgems
 
+class poe_gem:
+	#colour=''
+	#name=''
+	#gemtype=''
+	#tags=['']
+	def __repr__(self):
+		return str(self.name) + str(self.colour)
+	def __init__(self):
+		self.colour=''
+		self.name=''
+		self.support=False
+		self.tags=[]
+
+class poe_character:
+	def __init__(self):
+		self.rank=15001
+		self.account_name = ''
+		self.character_name = ''
+		self.character_view_url = ''
+		self.gems_used=[]
+		self.subclass=''
+		self.gemdump=[]
+		self.socketgroupsdump=[]
 
 
 items = open("C:\\items\\items.json", "r", encoding="utf-8")
@@ -23,10 +47,19 @@ linkdata=open("C:\\items\\linkdata.txt", "w+", encoding="utf-8")
 socketgroups=['','','','','','']
 items_json=json.load(items)
 socketgroups_data=[]
-#print(items_json[0]["character"]["name"])
-gems=""
+translation={"S" : "R", "D" : "G", "I" : "B", "W" : "W", "A": "A"}
+gems=[]
+characters=[]
 for rank, character in enumerate(items_json):
-	#print(f"Rank {rank}: {character['character']['name']}")
+	print(f"Rank {rank}: {character['character']['name']}")
+	tmp_character = poe_character()
+	tmp_character.rank = rank
+	tmp_character.character_name = character["character"]["name"]
+	tmp_character.subclass = character["character"]["class"]
+	response=requests.get(f"https://www.pathofexile.com/character-window/get-account-name-by-character?character={tmp_character.character_name}")
+	rjson=response.json()
+	if (response.status_code == 200):
+		tmp_character.account_name = rjson["accountName"]
 	for item in character['items']:
 		if ('sockets' in item):
 			for socket in item['sockets']:
@@ -34,28 +67,21 @@ for rank, character in enumerate(items_json):
 					socketgroups[socket['group']]+=socket['sColour']
 		if ('socketedItems' in item):
 			for gem in item['socketedItems']:
-				if(gem['colour']=='S'):	
-					gems+='R'
-				elif(gem['colour']=='D'):	
-					gems+='G'
-				elif(gem['colour']=='I'):	
-					gems+='B'
-				elif(gem['colour']=='W'):	
-					gems+='W'
-		if((gems) and socketgroups):
-			#print (f"Allocating {gems} into {socketgroups}")
-			#print(allocate_gems(socketgroups, gems))
-			for linkset in allocate_gems(socketgroups, gems):
-				if(len(linkset) >= 4):
-					actual_sorted_link_groups.append(linkset)
-			
-		gems=''
+				if('abyssJewel' in gem):
+					continue
+				tmp=poe_gem()
+				tmp.colour=translation.get(gem['colour'])	
+				tmp.name=gem['typeLine']
+				tmp.support=gem['support']
+				tmp.tags=gem['properties'][0]["name"]
+			gems.append(tmp)
+		tmp_character.socketgroupsdump.append(socketgroups)
+		tmp_character.gemdump.append(gems)
+		gems=[]
 		socketgroups=['','','','','','']
-#socketgroups_data=sorted(socketgroups_data)
+	characters.append(tmp_character)
 
-for datapoint in actual_sorted_link_groups:
-	linkdata.write(datapoint)
-	linkdata.write("\n")
-
-
-
+for i in range (3):
+	print (i)
+	for x in characters[i].gemdump:
+		print(x)
