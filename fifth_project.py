f641958554db7761a322328de8f4c0356a1cc10f
Andrey Osh <aoshukov@gmail.com>
2019-09-30 21:10:51 +0300

    fixing query syntax

--- fifth_project.py
+++ fifth_project.py
@@ -145,9 +145,9 @@ for rank, character in enumerate(items_json):
 		characters.append(tmp_character)
 print(f"AN {characters[0].account_name} CN {characters[0].character_name}")
 
-INSERT_CHAR_QUERY = "INSERT INTO characters (char_id, account_name, character_name) VALUES (%s %s %s);"
-INSERT_ITEM_QUERY = "INSERT INTO items (item_id, inventory_id, sorted_links, char_id) VALUES (%s %s %s %s);"
-INSERT_GEM_QUERY = "INSERT INTO gems (colour, name, support, tags, item_id, char_id) VALUES (%s %s %s %s %s %s);"
+INSERT_CHAR_QUERY = "INSERT INTO characters (char_id, account_name, character_name) VALUES (%s, %s, %s);"
+INSERT_ITEM_QUERY = "INSERT INTO items (item_id, inventory_id, sorted_links, char_id) VALUES (%s, %s, %s, %s);"
+INSERT_GEM_QUERY = "INSERT INTO gems (colour, name, support, tags, item_id, char_id) VALUES (%s, %s, %s, %s, %s, %s);"
 
 for char_id, char in enumerate(characters):
 	print ("running the following query:")
