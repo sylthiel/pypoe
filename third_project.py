40547507413333e01e7015ceb298ef012cac4891
Andrey Osh <aoshukov@gmail.com>
2019-09-30 18:04:32 +0300

    ad

--- third_project.py
+++ third_project.py
@@ -1,17 +1,27 @@
 ##GRABBER
-
 import json
 import requests
+import mysql.connector as sql
+
+rds_host='pypoe.cgqezikpygym.eu-west-2.rds.amazonaws.com'
+creds=open('creds.txt', 'r')
+pwd=(creds.read()).strip()
+
+con=sql.connect(host=rds_host, user='admin', password=pwd, database='pytest')
+cursor=con.cursor(buffered=True)
+
+insert_query="INSERT INTO Acc_Chr VALUES (%s, %s);"
+
 
 with open("grabbed.json", "r") as data_file:
 	character_list = json.load(data_file)
-list_of_acctchr=open("C:\\list_of_acctchr.txt", "w+", encoding="utf-8")
-char_count=0
+
 for i in range(len(character_list)):
 	for j in range (len(character_list[i]["entries"])):
 		acct_chr=("", "")
-		acct_chr=(str(character_list[i]["entries"][j]["character"]["name"]), str(character_list[i]["entries"][j]["account"]["name"]))
+		acct_chr=(str(character_list[i]["entries"][j]["account"]["name"]), str(character_list[i]["entries"][j]["character"]["name"]))
 		
-		list_of_acctchr.write(acct_chr[0] + "," +  acct_chr[1] + "\n")
+		con.execute(insert_query, acct_chr)
 		char_count += 1
-print (char_count, " characters written to list_of_acctchr.txt")
+
+con.commit()
