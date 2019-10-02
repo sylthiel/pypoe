a4098ecf6864cb7dfbc419820699935049ccfda4
Andrey Osh <aoshukov@gmail.com>
2019-10-02 15:01:05 +0300

    playing with timeouts

--- fourth_project.py
+++ fourth_project.py
@@ -53,7 +53,8 @@ for acct_chr in enumerate(list_of_acctchr):
 			attempts+=1
 		request+=1
 		dbg.write(f"[{datetime.datetime.now()}] Request iteration {request} for https://www.pathofexile.com/character-window/get-items?accountName={acct}&character={chr} with response code {response.status_code}\n")
+		time.sleep(1)
 		if (request >= requestlimit):
-			time.sleep(180)
+			time.sleep(15)
 			request=0
 json.dump(list_items, items, indent=4, ensure_ascii=False)
