22ea54897e386885dbbc7d4ec780d3d8d9c20c6c
Andrey Osh <aoshukov@gmail.com>
2019-10-03 18:54:59 +0300

    unfucked the spaces into tabs

--- data_handler.py
+++ data_handler.py
@@ -38,13 +38,13 @@ class poe_item:
 		return s
 class poe_character:
 	def __init__ (self, rank, account_name, character_name, subclass):
-        self.rank=rank
-        self.account_name = account_name
-        self.character_name = character_name
-        self.subclass=subclass
-        self.gemdump=[]
-        self.socketgroupsdump=[]
-        self.items=[]
+		self.rank=rank
+		self.account_name = account_name
+		self.character_name = character_name
+		self.subclass=subclass
+		self.gemdump=[]
+		self.socketgroupsdump=[]
+		self.items=[]
 
 
 
@@ -73,27 +73,27 @@ cursor=sql_connection.cursor(buffered=True)
 #db=open("grabbed.json", "w+", encoding='utf-8')
 dbg=open("dbg.log", "a+", encoding="utf-8")
 with open('computerid', 'r') as id:
-    computerid=int(id.read())
+	computerid=int(id.read())
 #
 ##please forgive me for using globalvariables
 item_id=0
 ##
 
 def yeet_character_items_to_sql(char):
-    print (f"{char_id} --  {char.account_name} -- {char.account_name}")
-    for item in char.items:
+	print (f"{char_id} --  {char.account_name} -- {char.account_name}")
+	for item in char.items:
 		cursor.execute(INSERT_ITEM_QUERY, (item_id, item.inventoryId, item.sortedlinks, char_id))
 		for gem in item.gems:
 			cursor.execute(INSERT_GEM_QUERY, (gem.colour, gem.name, gem.support, gem.tags, item_id, char_id))
 		item_id+=1
-    sql_connection.commit()
+	sql_connection.commit()
 def parse_api_character_items(api_character_items, rank, account, character):
-    socketgroups=[]
-    gems=[]
-    tmp_character=poe_character(rank, account, character, api_character_items["character"]["subclass"])
-    for item in api_character_items['items']:
+	socketgroups=[]
+	gems=[]
+	tmp_character=poe_character(rank, account, character, api_character_items["character"]["subclass"])
+	for item in api_character_items['items']:
 		tmp_item=poe_item()
-        has_items=0
+		has_items=0
 		if ('sockets' in item):
 			has_items=1
 			tmp_socketgroup=['','','','','','']
@@ -147,52 +147,52 @@ def obtain_ladder(current_league_url):
 		offset+=200
 	return json.dumps(list_grabbed, indent=4, ensure_ascii=False)
 def ladder_to_sql():
-    insert_query="INSERT INTO characters (account_name, character_name, char_id) VALUES (%s, %s, %s);"
-    character_list=json.loads(obtain_ladder(current_league_url))
-    
-    for i in range(len(character_list)):
-        for j in range (len(character_list[i]["entries"])):
-            acct_chr=(str(character_list[i]["entries"][j]["account"]["name"]), str(character_list[i]["entries"][j]["character"]["name"]), str(character_list[i]["entries"][j]["rank"]))
-            cursor.execute(insert_query, acct_chr)
-    sql_connection.commit()
+	insert_query="INSERT INTO characters (account_name, character_name, char_id) VALUES (%s, %s, %s);"
+	character_list=json.loads(obtain_ladder(current_league_url))
+	
+	for i in range(len(character_list)):
+		for j in range (len(character_list[i]["entries"])):
+			acct_chr=(str(character_list[i]["entries"][j]["account"]["name"]), str(character_list[i]["entries"][j]["character"]["name"]), str(character_list[i]["entries"][j]["rank"]))
+			cursor.execute(insert_query, acct_chr)
+	sql_connection.commit()
 def grab_items(request_limit=9001):
-    dbg.write("----------------------------------------------------------")
-    dbg.write(f"[{datetime.datetime.now()}] started working item data")
-    list_items=[]
-    cursor.execute(GET_CHARACTERS_QUERY, (int(computerid*5000 + 5000),))
-    list_of_acctchr=cursor.fetchall()
-    #print (list_of_acctchr[0][2])
-    #print (list_of_acctchr[1])
-    for acct_chr in (list_of_acctchr):
-        rank, acct, chr = acct_chr[0], acct_chr[1], acct_chr[2]
-        print (f"Reqest number {acct_chr[0]}")
-        print (f"https://www.pathofexile.com/character-window/get-items?accountName={acct}&character={chr}")
-        attempts=0
-        request=0
-        while(True):
-            if(attempts > 3): 
-                break
-            try:
-                response = requests.get(f"https://www.pathofexile.com/character-window/get-items?accountName={acct}&character={chr}")
-            except ConnectionError as err:
-                dbg.write(f"FATALLY FAILING ON {request} with {str(err)}. Sleeping for 180")
-                time.sleep(180)
-                continue
-            print(response.status_code)
-            if(response.status_code==200):
-                current_character_items=json.loads(response.json())
-                break
-            elif(response.status_code!=429):
-                break
-            attempts += 1 
-        request += 1
-        parse_api_character_items(current_character_items, rank, acct, chr)
-        dbg.write(f"[{datetime.datetime.now()}] Request iteration {request} for https://www.pathofexile.com/character-window/get-items?accountName={acct}&character={chr} with response code {response.status_code}\n")
-        time.sleep(2)
-        if(request >= request_limit):
-            break;
+	dbg.write("----------------------------------------------------------")
+	dbg.write(f"[{datetime.datetime.now()}] started working item data")
+	list_items=[]
+	cursor.execute(GET_CHARACTERS_QUERY, (int(computerid*5000 + 5000),))
+	list_of_acctchr=cursor.fetchall()
+	#print (list_of_acctchr[0][2])
+	#print (list_of_acctchr[1])
+	for acct_chr in (list_of_acctchr):
+		rank, acct, chr = acct_chr[0], acct_chr[1], acct_chr[2]
+		print (f"Reqest number {acct_chr[0]}")
+		print (f"https://www.pathofexile.com/character-window/get-items?accountName={acct}&character={chr}")
+		attempts=0
+		request=0
+		while(True):
+			if(attempts > 3): 
+				break
+			try:
+				response = requests.get(f"https://www.pathofexile.com/character-window/get-items?accountName={acct}&character={chr}")
+			except ConnectionError as err:
+				dbg.write(f"FATALLY FAILING ON {request} with {str(err)}. Sleeping for 180")
+				time.sleep(180)
+				continue
+			print(response.status_code)
+			if(response.status_code==200):
+				current_character_items=json.loads(response.json())
+				break
+			elif(response.status_code!=429):
+				break
+			attempts += 1 
+		request += 1
+		parse_api_character_items(current_character_items, rank, acct, chr)
+		dbg.write(f"[{datetime.datetime.now()}] Request iteration {request} for https://www.pathofexile.com/character-window/get-items?accountName={acct}&character={chr} with response code {response.status_code}\n")
+		time.sleep(2)
+		if(request >= request_limit):
+			break;
 
-    
+	
 print(datetime.datetime.now())
 ladder_to_sql()
 print(datetime.datetime.now())
